ed4a2fc25458f07b47604ec377ef157d9e8e1dce
mateusMSA2000 <97634285+mateusMSA2000@users.noreply.github.com>
2022-01-12 17:52:01 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var pokemon
+
+
+
+function setup() {
+  createCanvas(2000,1000);
+  pokemon = createSprite (200,200,150,150)
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites ()
+pokemon.velocityX = -5
+}
+
+
+
+
